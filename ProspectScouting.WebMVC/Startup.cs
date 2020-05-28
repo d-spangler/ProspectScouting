e56bebb5b9da8eff075698c6cace02e879b992be
d-spangler <61887496+d-spangler@users.noreply.github.com>
2020-05-28 14:51:17 -0400

    Add project files.

---
+++ ProspectScouting.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProspectScouting.WebMVC.Startup))]
+namespace ProspectScouting.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
